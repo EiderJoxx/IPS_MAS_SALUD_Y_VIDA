7d4d36fc93bcfbe06cba79d6c96389e428e2a9d5
CristianSanz09 <101913370+CristianSanz09@users.noreply.github.com>
2023-09-22 11:14:00 -0500

    primer commit prueba

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPS_SALUD_MAS_VIDA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estamos trabajando en como solucionar git");
+            Console.ReadKey();
+        }
+    }
+}
